31304421ba2b9a4b9caa3f5c6238a27033e91e0c
tonnkatonno <tonnkatonno@gmail.com>
2025-05-20 08:01:27 +0200

    add GameConstans.fs

---
+++ PixelTacToe.Server/Helpers/GameConstants.fs
@@ -0,0 +1,11 @@
+namespace PixelTacToe.Server.Constants
+
+module GameConstants =
+    [<Literal>]
+    let WebSocketEndpoint = "/api/ws"
+    
+    [<Literal>]
+    let BoardSize = 3
+    
+    [<Literal>]
+    let DefaultPlayerImage = "/uploads/blank.png" 
